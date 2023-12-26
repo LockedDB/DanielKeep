e851599353ffe814e71c45a2c20404493b33a913
Daniel <daniel.benet@outlook.es>
2023-12-26 10:39:21 +0100

    Initial Commit

---
+++ DanielKeep/DanielKeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanielKeepApp.swift
+//  DanielKeep
+//
+//  Created by Dani Benet on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DanielKeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
