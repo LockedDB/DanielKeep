42a0a138868058d2cea04775ef6a437d8711db27
Daniel BENET <daniel.benet@outlook.es>
2023-12-28 17:37:26 +0100

    Simple Card Design
    Prepare the cards for the next step in which we add colours to them.

--- DanielKeep/Views/ContentView.swift
+++ DanielKeep/Views/ContentView.swift
@@ -6,17 +6,13 @@ struct ContentView: View {
     @State private var isAddNoteSheetPresented = false
     @Environment(\.modelContext) var context
     
+
+    
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(notes) {
-                    Text($0.title)
-                }
-                .onDelete { indexSet in
-                    for index in indexSet {
-                        context.delete(notes[index])
-                    }
-                }
+            
+            ScrollView {
+                cardsGrid()
             }
             .navigationTitle("Notes")
             .navigationBarTitleDisplayMode(.large)
@@ -25,7 +21,7 @@ struct ContentView: View {
                 if !notes.isEmpty {
                     Button("Add Note", systemImage: "plus") {
                         isAddNoteSheetPresented = true
-                    }                    
+                    }
                 }
             }
             .overlay {
@@ -41,7 +37,66 @@ struct ContentView: View {
                 }
             }
         }
+        
     }
 }
 
-#Preview { ContentView() }
+extension ContentView {
+    
+    
+    private func cardsGrid() -> some View {
+        
+        let columns = [
+            GridItem(.flexible(), spacing: nil, alignment: .topLeading),
+            GridItem(.flexible(), spacing: nil, alignment: .topLeading)
+        ]
+        
+        return LazyVGrid(columns: columns) {
+            ForEach(notes) { note in
+                noteCard(note: note)
+            }
+        }
+        .padding(.horizontal, 8)
+    }
+    
+    private func noteCard(note: Note) -> some View {
+        return VStack(alignment: .leading) {
+            Text(note.title)
+            if !note.content.isEmpty {
+                Text(note.content)
+                    .font(.body)
+                    .fontWeight(.light)
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .topLeading)
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(.ultraThinMaterial, lineWidth: 1.5)
+        )
+    }
+}
+
+#Preview {
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: Note.self, configurations: config)
+    
+    
+    var note = Note(title: "Note", content: "Empty Note")
+    container.mainContext.insert(note)
+    
+    note = Note(title: "Example Note title bit longer", content: "Example content")
+    container.mainContext.insert(note)
+    
+    note = Note(title: "Shorther ", content: "Example content")
+    container.mainContext.insert(note)
+    
+    note = Note(title: "Example Note extra large", content: "Example content with a super long description omg where is this going to end.")
+    container.mainContext.insert(note)
+    
+    note = Note(title: "Example Note", content: "Example content")
+    container.mainContext.insert(note)
+    
+    return ContentView()
+        .modelContainer(container)
+}
